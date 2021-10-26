ba6784557419dc02725cef6ea3e89dda823f63e5
Kay <kyv.hardeman@gmail.com>
2021-10-26 13:59:57 -0400

    vowels complete

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,26 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//   let vowels = ["a", "e", "i", "o", "u"];
+//   let count = 0;
+//   for (let char of str.toLowerCase()) {
+//     if (vowels.includes(char)) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
+// alternate solution - condensed
+// matches eiter returned the matches in an array or it returns null
+function vowels(str) {
+  let matches = str.match(/[aeiuo]/gi); //if this str contains any character inside of our brackets
+
+  return matches ? matches.length : 0; //if matches is a truthy value return the length of that array if it is falsy or null return 0 there are no matches
+}
+// regex
+// g means do not stop at the first match
+// i means case insenstive
 
 module.exports = vowels;
