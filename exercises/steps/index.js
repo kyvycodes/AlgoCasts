35fdc02c9eaaabc1600683e85b645b13fca1e4d8
Kay <kyv.hardeman@gmail.com>
2021-10-28 09:00:42 -0400

    steps complete

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,50 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+//n is the amount of levels
+//there must be a space char to the left, the number will always be squared
+//log each independent level
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let step = "";
+
+//     for (let col = 0; col < n; col++) {
+//       if (col <= row) {
+//         step += "#";
+//       } else {
+//         step += " ";
+//       }
+//     }
+//     // after we process one entire row - one console log for each row
+//     console.log(step);
+//   }
+// }
+
+// recursive solution
+function steps(n, row = 0, stair = "") {
+  //start on top row of problem
+
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1); // stops the current run
+  }
+  // on first call stair.length is 0, less so we enter this condition - both are 0
+  if (stair.length <= row) {
+    stair += "#"; //enter
+  } else {
+    stair += " "; //second function call stair is greater, add the empty space
+  }
+
+  // then call the function again
+  steps(n, row, stair);
+}
+
+// another option - turn if statement to ternary
+// const add = stair.length <= row ? '#' : ' ';
+// steps(n, row, stair + add);
 
 module.exports = steps;
