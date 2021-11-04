a1f321f909f88422e2b15c48e58dcf01a6f540e7
Kay <kyv.hardeman@gmail.com>
2021-11-03 22:40:09 -0400

    qfroms

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,50 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+//take a stack FILO to a queue FIFO
+class Queue {
+// need two stacks two work with
+constructor(){
+  this.first = new Stack()
+  this.second = new Stack()
+}
+
+add(element){
+  this.first.push(element)
+}
+
+remove(){
+  while(this.first.peek()){ //as long as there is still a value to operate on
+    this.second.push(this.first.pop())
+  }
+  //when this is done all the elements have been moved over to second stack
+  let element = this.second.pop()
+
+
+  //then return the other values back to the original stack
+  while(this.second.peek()){
+    this.first.push(this.second.pop())
+   }
+   return element
+  }
+
+  peek(){
+    while(this.first.peek()){ //as long as there is still a value to operate on
+      this.second.push(this.first.pop())
+    }
+
+    //just store what the last element is, without removing it
+    let element = this.second.peek()
+
+
+    //then return the other values back to the original stack
+    while(this.second.peek()){
+      this.first.push(this.second.pop())
+     }
+     return element
+    }
+
+
+}
 
 module.exports = Queue;
