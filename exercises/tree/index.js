ee867780a3fe0433729f3638743e925af2b7cd4d
Kay <kyv.hardeman@gmail.com>
2021-11-03 23:32:11 -0400

    tree complete

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,29 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  //node carries references to every child node
+  constructor (data){ //agr that gets assigned to the data property
+    this.data = data;
+    this.children = [] //empty array for storing children - every single need node will have the ability to add a child node
+  }
 
-class Tree {}
+  add(data){
+    const node = new Node(data)//create a new node and push it to the childrens array
+    this.children.push(node) // or this.children.push(new Node())
+  }
+
+  remove(data){ // checks the data against children nodes and removes the node that houses that value
+    this.children = this.children.filter((node) => {
+      return node.data !== data;
+    })
+  }
+}
+
+class Tree {
+  constructor(){
+    this.root = null;
+  }
+}
 
 module.exports = { Tree, Node };
