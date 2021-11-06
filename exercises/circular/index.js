621af6dcd1725c2bd6e0e20f351feb9fe140cc83
Kay <kyv.hardeman@gmail.com>
2021-11-06 00:59:58 -0400

    circular complete

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,23 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+
+//last nod points to an earlier list
+function circular(list) {
+  let slow = list.head
+  let fast = list.head
+
+
+  while(fast.next && fast.next.next){
+    slow = slow.next
+    fast = fast.next.next
+
+    if(slow === fast) {// if it is the same object in memory, the identical node not the data (it is possible for the list to have a duplicate data values)
+      return true
+   }
+  }
+  return false //we do not have a circular list
+}
+
 
 module.exports = circular;
