132bd10cdc8a73a52f273b019fe6cd347e55a793
Kay <kyv.hardeman@gmail.com>
2021-11-06 01:21:53 -0400

    fromlast complete

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,24 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) { //FROM the last (0 spaces from the last would be the last)
+let slow = list.head
+let fastAtN = list.head
+
+//step 1: advance the pointer thru n elements
+while(n > 0){
+  fastAtN = fastAtN.next
+  n--
+}
+
+//step: 2 advance both element by one, exit while loop when fast is pointing at the last element
+while(fastAtN.next){
+  slow = slow.next
+  fastAtN = fastAtN.next
+}
+
+//slow will be pointing n elements behind it
+  return slow
+}
 
 module.exports = fromLast;
