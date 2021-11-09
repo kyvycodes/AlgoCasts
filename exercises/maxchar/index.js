15e8065b06c3c3f392522c8dd277c0a3331a0294
Kay <kyv.hardeman@gmail.com>
2021-11-09 11:55:00 -0500

    maxchar w comments

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,14 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+// Steps
+
+// to create a obj to map
+  // create an empty object
+  // loop through your data
+  // if the key is not present set the value to one (!key[value] = 1)
+  // else up the current count (key[value]++)
+
 function maxChar(str) {
   let chars = {};
 
@@ -18,20 +26,27 @@ function maxChar(str) {
   }
 
   // iterate thru object
-  let max = 0; //to keep track of the value
-  let maxChar = " "; //to keep track of the key
+  let max = 0; //keep track of the value
+  let maxKey = " "; //to keep track of the key
 
-  for (let ch in chars) {
-    //ch is the key of the obj not the values
-    if (chars[ch] > max) {
-      max = chars[ch];
-      maxChar = ch;
+  //iterate thou the
+  for (let key in chars) {
+
+    // if the key in the chars object is greater than the max
+    // replace the max
+    // set the maximum character to the actual key
+    if (chars[key] > max) {
+      max = chars[key];
+      maxKey = key;
     }
   }
-  return maxChar;
+
+  return maxKey; //that represents max value
 }
 
-//for of used to iterate thru an array or a string or any type of iterable object
-//for in is used to iterate thru an actual object - a set of key value pairs
-
 module.exports = maxChar;
+
+
+// notes:
+// for of used to iterate thru an array or a string or any type of iterable object
+// for in is used to iterate thru an actual object - a set of key value pairs
