393145db419254f2177fee2f4d07ecaf77c99e81
Kay <kyv.hardeman@gmail.com>
2021-11-09 11:19:27 -0500

    steps w comments

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,50 +17,37 @@
 //       '### '
 //       '####'
 
-//n is the amount of levels
-//there must be a space char to the left, the number will always be squared
-//log each independent level
-// function steps(n) {
-//   for (let row = 0; row < n; row++) {
-//     let step = "";
-
-//     for (let col = 0; col < n; col++) {
-//       if (col <= row) {
-//         step += "#";
-//       } else {
-//         step += " ";
-//       }
-//     }
-//     // after we process one entire row - one console log for each row
-//     console.log(step);
-//   }
-// }
 
 // recursive solution
-function steps(n, row = 0, stair = "") {
-  //start on top row of problem
+// initialize default parameters one for the rows starting at zero (the top row) and one for the stairs ''
+// base case - if n is the size of the row return the problem
+// if n is the step length log the step and stop the current run
+//
+function steps(n, row = 0, step = "") {
 
   if (n === row) {
     return;
   }
 
-  if (n === stair.length) {
-    console.log(stair);
+  if (n === step.length) {
+    console.log(step);
     return steps(n, row + 1); // stops the current run
   }
   // on first call stair.length is 0, less so we enter this condition - both are 0
-  if (stair.length <= row) {
-    stair += "#"; //enter
+  if (step.length <= row) {
+    step += "#"; //enter
   } else {
-    stair += " "; //second function call stair is greater, add the empty space
+    step += " "; //second function call stair is greater, add the empty space
   }
 
   // then call the function again
   steps(n, row, stair);
 }
 
-// another option - turn if statement to ternary
-// const add = stair.length <= row ? '#' : ' ';
-// steps(n, row, stair + add);
 
 module.exports = steps;
+
+
+// another option - turn if statement to ternary
+  // const add = stair.length <= row ? '#' : ' ';
+  // steps(n, row, stair + add);
